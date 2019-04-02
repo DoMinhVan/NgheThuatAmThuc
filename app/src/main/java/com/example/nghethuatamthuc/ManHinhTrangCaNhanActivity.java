5c7f4d99f50f70e2f1da39f47cfd966e72799584
DoMinhVan <43976823+DoMinhVan@users.noreply.github.com>
2019-04-02 16:11:27 +0700

    Giao diện màn hình trang cá nhân

---
+++ app/src/main/java/com/example/nghethuatamthuc/ManHinhTrangCaNhanActivity.java
@@ -0,0 +1,13 @@
+package com.example.nghethuatamthuc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManHinhTrangCaNhanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.man_hinh_trang_ca_nhan_layout);
+    }
+}
