0a8871a72abe058299fab55dd3b0a3afcc5a118f
DoMinhVan <43976823+DoMinhVan@users.noreply.github.com>
2019-04-02 16:23:57 +0700

    Giao diện màn hình trang cá nhân

--- app/src/main/java/com/example/nghethuatamthuc/ManHinhTimKiemActivity.java
+++ app/src/main/java/com/example/nghethuatamthuc/ManHinhTimKiemActivity.java
@@ -8,9 +8,11 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ManHinhTimKiemActivity extends AppCompatActivity {
     Toolbar toolbarTimKiem;
+    Spinner spDanhMuc,spDoiTuong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,25 @@ public class ManHinhTimKiemActivity extends AppCompatActivity {
         setSupportActionBar(toolbarTimKiem);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(null);
+        //Spinner
+        //danh muc
+        spDanhMuc = (Spinner) findViewById(R.id.spDanhMuc);
+        List<String> list = new ArrayList<String>();
+        list.add("Món Lẩu");
+        list.add("Món Nướng");
+        list.add("Món Khai vị");
+        ArrayAdapter<String> adapterSpinner = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item, list);
+        adapterSpinner.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        spDanhMuc.setAdapter(adapterSpinner);
+        //doi tuong
+        spDoiTuong = (Spinner) findViewById(R.id.spDoiTuong);
+        List<String> list2 = new ArrayList<String>();
+        list2.add("Bình Thường");
+        list2.add("Ăn Kiêng");
+        list2.add("Người Bệnh Tiểu Đường");
+        ArrayAdapter<String> adapterSpinner2 = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item, list2);
+        adapterSpinner.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        spDoiTuong.setAdapter(adapterSpinner2);
     }
 
     @Override
